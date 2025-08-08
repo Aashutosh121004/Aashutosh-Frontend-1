a9aa1bed7f110bad9b9c7836b409cfaa98dcac0d
Aashutosh121004 <AASHUTOSH.GUPTA012@GMAIL.COM>
2025-08-08 21:16:03 +0530

    Add files via upload

---
+++ fat_arrow_func.js
@@ -0,0 +1,9 @@
+let sum = function(x,y){return x+y;};
+
+console.log(sum(5,10));
+
+let sumArrow = (x,y) => { return x+y; };
+console.log(sumArrow(5,10));
+
+let square = x => x*x;
+
---
+++ intro_1.js
@@ -0,0 +1,2 @@
+var result = "hello"/2;
+console.log(result); // This will log NaN since dividing a string by a number is not valid
