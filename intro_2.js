2da872abe40d240433dbbbd877bbbcd002738982
Aashutosh121004 <AASHUTOSH.GUPTA012@GMAIL.COM>
2025-08-11 15:20:33 +0530

    Update intro_2.js

---
+++ intro_2.js
@@ -0,0 +1,22 @@
+/*
+VARIABLES:
+Variable is a container to store data values.
+value can be of any data type: number, string, boolean, array, object, function, etc.
+Variable name must be meaningful and & camel case will not accept special characters except underscore(_)& $
+
+
+VARIABLE DECLARATION:
+1. Var: global scope  and redeclaration is allowed , and obviously value can be changed
+2. Let: block scope and redeclaration is not allowed, but value can be changed
+3. Const: block scope and redeclaration is not allowed, and value cannot be changed once assigned
+
+
+CONCATENATION:
+Concatenation is the process of joining two or more strings together.
+let str1 = "Hello";
+let str2 = "World";
+let result = str1 + " " + str2; // result will be "Hello World"
+
+
+
+*/
