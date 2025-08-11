d6009df52dcc2fe0f1b8160adc0744bcd0afbb18
Aashutosh121004 <AASHUTOSH.GUPTA012@GMAIL.COM>
2025-08-11 15:20:04 +0530

    Update intro_1.js

--- intro_1.js
+++ intro_1.js
@@ -1,2 +1,51 @@
-var result = "hello"/2;
-console.log(result); // This will log NaN since dividing a string by a number is not valid
+/*
+what is javascript? 
+javascript is a programming language that allows you to implement complex features on web pages. 
+It is an essential part of web development, alongside HTML and CSS.
+JavaScript enables interactive web pages and is an essential part of web applications. It is a versatile
+js is used to create dynamic content, control multimedia, animate images, and much more.
+JavaScript is an interpreted language, meaning it is executed line by line at runtime, 
+which allows for quick development and testing.
+It can be used on both the client-side (in the browser) and server-side (with Node.js).
+JavaScript supports object-oriented, imperative, and functional programming styles.
+JavaScript is a high-level, dynamic, untyped, and interpreted programming language.
+JavaScript is often used in conjunction with HTML and CSS to create interactive web applications.
+It is a prototype-based language, meaning it uses prototypes rather than classes for inheritance.
+js can be used for both front-end and back-end development, making it a full-stack language.
+
+key components of javascript:
+1. scripting language : line by by line execution 
+2. interpreted language : no need to compile or u mean no need to install a compiler , we can use browser console or serve for node.js
+3. case sensitive 
+4. object based language : everything is an object in js
+5. single threaded : js runs in a single thread, meaning it can only execute one command at a time, but it can handle asynchronous operations.
+6. allow aynchronous functions : js can handle asynchronous operations using callbacks, promises, and async/await in ES6
+7. Event driven architecture: event handling is a key feature of JavaScript, allowing developers to create interactive applications that respond to user actions.
+
+
+
+How to use js?
+internal: using <script> tag in HTML where we write js code directly in between the tags.
+external: using <script src="path_to_js_file.js"></script> tag in HTML to link an external JavaScript 
+file where our js code is written.
+inline: using the onclick attribute in HTML elements to execute JavaScript code when an event occur i.e event handling
+
+
+
+
+PRINT STATEMENT
+console.log("Hello, World!"); // This will print "Hello, World!" to the console
+document.write("Hello, World!"); // This will write "Hello, World!" to the HTML document
+alert("Hello, World!"); // This will show an alert box with "Hello, World!" , this is a pop up message
+
+
+
+DATA-TYPES in JS:
+i) Prementive Data Types: number(12,1.2), string(alphabet,alphanumeric,character | '',""), 
+boolean(true,false), undefined(when we declared variable but didn't assign any value), null
+ii) Complex Data Types: array, object, JSON(Javascript Object Notation) – object
+
+How to check datatypes?
+typeof – keyword
+
+*/
