95c23909ffce5c76da31f75fa577af223095dd29
Aashutosh121004 <AASHUTOSH.GUPTA012@GMAIL.COM>
2026-03-03 19:51:13 +0530

    js functions and loop part1

--- into_3.js
+++ into_3.js
@@ -33,23 +33,18 @@
 
 
 
-const userName = prompt("May I know your name?");
 
+// Take input from user on dialog box , then showing output and functionality.
+const userName = prompt("May I know your name?");
 // Print a welcome message using template literals
 // ${userName} inserts the value inside the string
 console.log(`Welcome, ${userName}!`);
-
-
 // Ask the user for their age
 // The input will be stored as a STRING by default
 const userAge = prompt('May I know your age?');
-
-
 // Convert the age (string) into a number
 // Number() tries to convert the string into a numeric value
 const userAgeNumber = Number(userAge);
-
-
 // Check if the conversion was successful
 // isNaN() means "is Not a Number"
 // If the value is NOT NaN → it means conversion worked
@@ -70,3 +65,121 @@ if (!isNaN(userAgeNumber)) {
     // Show error message
     console.log('Please enter a valid number for age.');
 }
+
+
+
+
+
+// if - elseif - else - conditonal statement
+const temp = 25;
+if (temp > 30) {
+    console.log("It's hot.");
+} else if (temp >= 20) {
+    console.log("It's warm.");
+} else {
+    console.log("It's cold.");
+}
+
+// output - Its Warm
+
+
+
+
+//switch case statement
+const day = "Monday";
+switch (day) {
+    case "Monday":
+        console.log("Start of the week.");
+        break;
+    case "Friday":
+        console.log("End of the workweek.");
+        break;
+    default:
+        console.log("It's a regular day.");
+}
+
+// output - Start of the week.
+
+
+
+
+
+
+
+// javascript ternary operator - conditional operator
+let a=10;
+console.log(a===5 ? "a is equal to 5":"a is not equal to 5");
+// output - a is not equal to 5
+// first thing is the comparison , if a===5 is yes then it will execute the first statement otherwise it will execute the second statement.
+
+
+
+
+// LOOPS
+
+
+// for loop
+// JavaScript for loop is a control flow statement that allows code to be executed repeatedly based on a condition. It consists of three parts: initialization, condition, and increment/decrement.
+
+// Syntax
+
+//              for (statement 1 ; statement 2 ; statement 3){    code here...}
+// Statement 1: It is the initialization of the counter. It is executed once before the execution of the code block.
+// Statement 2: It defines the testing condition for executing the code block
+// Statement 3: It is the increment or decrement of the counter & executed (every time) after the code block has been executed.
+// Now let's understand this with the help of an example
+
+for(let x=1; x<=5; x++)
+{
+    console.log("value of is x:" + x);
+
+}
+
+
+
+
+
+//MORE LOOPS IN JAVASCRIPT
+
+// JavaScript has different kinds of loops in Java. Some of the loops are:
+// Loop	Description
+// for loop	A loop that repeats a block of code a specific number of times based on a conditional expression.
+// while loop	A loop that repeats a block of code as long as a specified condition is true.
+// do-while loop	A loop that executes a block of code at least once, then repeats the block as long as a specified condition is true.
+// for...of loop	Iterates over the values of an iterable object (like arrays, strings, maps, sets, etc.)
+// for...in loop	Iterates over the enumerable properties of an object (including inherited properties).
+// Learn and master JavaScript with Practice Questions. JavaScript Exercises provides many JavaScript Exercise questions to practice and test your JavaScript skills.
+
+
+
+// WHILE LOOP
+//The while loop executes a block of code as long as a specified condition is true. In JavaScript, this loop evaluates the condition before each iteration and continues running as long as the condition remains true.
+
+let count = 1;
+while (count <= 5) {
+  console.log(count);
+  count++;
+}
+
+// Using While Loop to find Traverse an Array
+
+let arr= [10, 20, 30, 40, 50];
+let i=0;
+while(i<arr.length)
+{
+    console.log(arr[i]);
+    i++;
+}
+
+
+
+
+
+// DO WHILE LOOP 
+//A Do-While loop is another type of loop in JavaScript that is similar to the while loop, but with one key difference: the do-while loop guarantees that the block of code inside the loop will be executed at least once, regardless of whether the condition is initially true or false .
+
+let count2 = 1;
+do {
+  console.log(count2);
+  count2++;
+} while (count2 <= 5);
--- into_4.js
+++ into_4.js
@@ -1,6 +1,119 @@
-// let age = 40;
-// console.log(age <50 ? "You are young" : "You are old"); // You are youngs
+// FUNCTION IN JAVASCRIPT
 
-// for ??
-let fn;
-console.log(fn ?? "enter a valid value please")
+// Functions in JavaScript are reusable blocks of code designed to perform specific tasks. They allow you to organize, reuse, and modularize code. It can take inputs, perform actions, and return outputs.
+function greet(name) {   // 'name' is a parameter
+  console.log("Hello " + name);
+}
+
+greet("Alice");  // "Alice" is the argument
+// OUTPUT - Hello Alice
+
+
+
+//Default Parameters
+// Default parameters are used when no argument is provided during the function call.
+// If no value is passed, the function automatically uses the default value.
+function greet(name = "Aashutosh") {
+  console.log("Hello, " + name);
+}
+
+greet();
+greet("Aman");
+
+
+
+//Return Statement
+// The return statement is used to send a result back from a function.
+// When return executes, the function stops running at that point.
+// The returned value can be stored in a variable or used directly.
+
+function add(a, b) {
+  return a + b; // returns the sum
+}
+
+let result = add(5, 10);
+console.log(result); // OUTPUT - 15
+
+
+
+// Type of Functions
+// Here are all the main types of functions in JavaScript:
+
+
+// 1. Named Function
+// A function that has its own name when declared. It’s easy to reuse and debug because the name shows up
+//  in error messages or stack traces.
+
+function greet(){
+    return "Hello, World!";
+}
+
+console.log(greet()); // OUTPUT - Hello, World!
+
+// 2. Anonymous Function
+// A function that does not have a name. 
+// It is usually assigned to a variable or used as a callback. Since it has no name, it cannot be called directly.
+
+const greet = function() {
+  return "Hi there!";
+};
+console.log(greet()); // OUTPUT - Hi there!
+
+// callbacks are very important part in this project making journey , and in that we use this type of function a lot.
+
+
+
+
+
+//3. Function Expression
+// When you assign a function (can be named or anonymous) to a variable. The function can then be used by calling that variable.
+const add = function(a, b) {
+  return a + b;
+};
+console.log(add(2, 3));
+
+
+
+
+//4. Arrow Function (ES6)
+// A new way to write functions using the => syntax. They are shorter and do not have their own this binding, 
+// which makes them useful in some cases.
+
+const square = n => n * n;
+console.log(square(4)); // OUTPUT - 16
+
+
+
+
+//5. Immediately Invoked Function Expression (IIFE)
+// IIFE functions are executed immediately after their definition. They are often used to create isolated scopes.
+(function () {
+    console.log("This runs immediately!");
+})();
+
+
+
+
+
+//6. Callback Functions
+// A callback function is passed as an argument to another function and is executed after the completion of that function.
+
+function num(n, callback) {
+    return callback(n);
+}
+
+const double = (n) => n * 2;
+
+console.log(num(5, double));
+
+
+
+
+
+
+
+
+
+
+
+
